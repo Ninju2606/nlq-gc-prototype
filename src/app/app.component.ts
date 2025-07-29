5ef5073d4e30de2dbfa8f4cfb43b5298ee38fe1f
Richard Vierhaus <ninju2606@gmail.com>
2025-07-29 19:52:18 +0200

    Initial Commit. Receiving GCs works

---
+++ src/app/app.component.ts
@@ -0,0 +1,126 @@
+import { AfterViewInit, Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, NgModel } from '@angular/forms';
+import { HttpCommunicatorService } from './http-communicator.service';
+import { Observable } from 'rxjs';
+import { HttpClientModule, HttpResponse } from '@angular/common/http';
+
+export interface Prompt {
+  key: string;
+  prompt: string;
+}
+
+export interface GraphCode {
+  state: string;
+  model: string;
+  dictionary: string[];
+  matrix: number[][];
+  error: string;
+  description: string;
+  start: number;
+}
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatIconModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements AfterViewInit {
+
+  title = 'NLQ-GC-Prototyp';
+  promptGCSelected: string = '';
+  promptKeywordSelected: string = '';
+  llmSelected: string = '';
+  query: string = '';
+  username: string = '';
+
+  loading: boolean = false;
+
+  promptsGC: Prompt[] = [];
+  promptsKeywords: Prompt[] = [];
+  llms: String[] = [];
+
+  currentGraphCode: GraphCode | null = null;
+
+
+  constructor(private httpService: HttpCommunicatorService) {
+  }
+
+  ngAfterViewInit() {
+    this.parsePrompts(this.httpService.getPromptsGC(), this.promptsGC);
+    this.parsePrompts(this.httpService.getPromptsKeyword(), this.promptsKeywords);
+    this.parseLLMs(this.httpService.getLLMs());
+  }
+
+  public async transmitQuery(): Promise<void> {
+    if (this.query.length == 0 || this.username.length == 0) {
+      alert("Bitte gib eine Anfrage sowie einen Nutzernamen an!");
+      return;
+    }
+    this.loading = true;
+    this.currentGraphCode = null;
+    var response = this.httpService.sendRequest(this.query, this.username, this.promptGCSelected, this.promptKeywordSelected, this.llmSelected);
+    var transactionId = '';
+    response.subscribe({
+      next: res => {
+        transactionId = JSON.parse(JSON.stringify(res.body))['transactionId'];
+      },
+      complete: () => {
+        if (transactionId.length !== 0) setTimeout(() => this.pollTransaction(transactionId), 1000);
+        else this.loading = false;
+      }
+    });
+  }
+
+  private pollTransaction(transactionId: string) {
+    var response = this.httpService.checkTransaction(transactionId);
+    var graphCode: GraphCode | null = null;
+    response.subscribe({
+      next: res => {
+        graphCode = JSON.parse(JSON.stringify(res.body));
+        console.log(graphCode);
+      },
+      complete: () => {
+        if (graphCode == null || graphCode.state == "NOT_AVAILABLE") {
+          alert("Es gab einen Fehler bei der GraphCode-Verarbeitung");
+          this.loading = false;
+        }
+        else if (graphCode.state == "PENDING") setTimeout(() => this.pollTransaction(transactionId), 1000);
+        else {
+          this.loading = false;
+          if (graphCode.state == "FINISHED") this.currentGraphCode = graphCode;
+          else alert(graphCode.error)
+        }
+      }
+    });
+  }
+
+  private parsePrompts(input: Observable<HttpResponse<string>>, list: Prompt[]): void {
+    input.subscribe({
+      next: response => {
+        var inputParsed = JSON.parse(JSON.stringify(response.body));
+        Object.keys(inputParsed).forEach(key => {
+          var current = {} as Prompt;
+          current.key = key;
+          current.prompt = inputParsed[key];
+          list.push(current);
+        });
+      }
+    });
+  }
+
+  private parseLLMs(input: Observable<HttpResponse<string>>) {
+    input.subscribe({
+      next: response => {
+        var inputParsed = JSON.parse(JSON.stringify(response.body));
+        inputParsed.forEach((entry: String) => this.llms.push(entry));
+      }
+    });
+  }
+
+}
---
+++ src/app/http-communicator.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders, HttpResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HttpCommunicatorService {
+
+    readonly apiUrl = "http://localhost:8080";
+
+    constructor(private http: HttpClient) { }
+
+    public getPromptsGC(): Observable<HttpResponse<string>> {
+        return this.http.get<string>(this.apiUrl + "/enums/promptsGC", { observe: 'response' });
+    }
+
+    public getPromptsKeyword(): Observable<HttpResponse<string>> {
+        return this.http.get<string>(this.apiUrl + "/enums/promptsKeyword", { observe: 'response' });
+    }
+
+    public getLLMs(): Observable<HttpResponse<string>> {
+        return this.http.get<string>(this.apiUrl + "/enums/models", { observe: 'response' });
+    }
+
+    public sendRequest(query: string, user: string, promptGC: string, promptKeyword: string, llm: string): Observable<HttpResponse<string>> {
+        var url = this.apiUrl + `/handleNLQ?query=${query}&user=${user}&promptKeyword=${promptKeyword}&promptGC=${promptGC}&model==${llm}`;
+        return this.http.post<string>(url, {}, { observe: 'response' });
+    }
+
+    public checkTransaction(transactionId: string): Observable<HttpResponse<string>> {
+        var url = this.apiUrl + `/graphCode?transactionId=${transactionId}`;
+        return this.http.get<string>(url, { observe: 'response' });
+    }
+
+}
